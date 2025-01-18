22e75ca56be21f9507e8c63b515e711b91e3e3de
dApY3112 <98270020+dApY3112@users.noreply.github.com>
2025-01-18 04:22:23 +0200

    Update test_auth.py

--- src/tests/test_auth.py
+++ src/tests/test_auth.py
@@ -1,4 +1,8 @@
 # src/tests/test_auth.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 from src.security.auth import generate_jwt, decode_jwt
 
 def test_jwt_generation():
