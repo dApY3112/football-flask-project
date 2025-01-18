53cf04391271463da54dba9f06e31ba7435830a6
dApY3112 <98270020+dApY3112@users.noreply.github.com>
2025-01-18 04:30:03 +0200

    Update run_tests.bat

--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -1,13 +1,18 @@
 @echo off
+REM Set the PYTHONPATH to the src directory (relative to the location of the script)
 set PYTHONPATH=%CD%\src
 
+REM Check the current directory for debugging purposes
 echo Current directory: %CD%
 echo PYTHONPATH: %PYTHONPATH%
 
+REM Use an absolute path to requirements.txt if it's in the root folder
 echo Installing dependencies...
-pip install -r ..\requirements.txt
+pip install -r C:\ProgramData\Jenkins\.jenkins\workspace\football-backend\requirements.txt
 
+REM Run the tests
 echo Running tests...
 pytest --maxfail=1 --disable-warnings -q
 
+REM Pause for debugging (optional, remove if not needed)
 pause
